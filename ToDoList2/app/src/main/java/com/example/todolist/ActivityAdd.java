074e4b460e94b072834530c238c2499d8abb4e72
diddbsgh <yangyoonho0710@naver.com>
2023-10-05 16:55:33 +0900

    ADD::ToDoList

--- ToDoList2/app/src/main/java/com/example/todolist/ActivityAdd.java
+++ ToDoList2/app/src/main/java/com/example/todolist/ActivityAdd.java
@@ -3,12 +3,19 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.todolist.databinding.ActivityAddBinding;
 
 public class ActivityAdd extends AppCompatActivity {
+
+    private ActivityAddBinding binding;   // Binding 객체 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        binding = ActivityAddBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
