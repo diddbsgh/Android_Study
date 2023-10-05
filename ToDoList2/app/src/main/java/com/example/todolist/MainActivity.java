477bf271417ceddebbcd98e0db86d35a23a04053
diddbsgh <yangyoonho0710@naver.com>
2023-10-05 16:46:07 +0900

    ADD::TodoList

--- ToDoList2/app/src/main/java/com/example/todolist/MainActivity.java
+++ ToDoList2/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.example.todolist.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;  // Binding 객체 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // btn_add 버튼 클릭 시 이벤트 처리
+        binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // activity_main -> activity_add로 화면 전환
+                Intent intent = new Intent(MainActivity.this, ActivityAdd.class);
+                startActivity(intent);
+            }
+        });
     }
 }
